26fd1a35bd7f71ee7222053e22856e5d36b2bf51
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-03-21 21:29:25 -0500

    ping fix

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Display info about this server.'),
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		return interaction.reply('Pong!');
 	},
 };
