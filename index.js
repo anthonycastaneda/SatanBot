ccffbabf65b63b1e88fa8a932f7d54552e640f15
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-03-23 10:33:17 -0500

    lots of settings and functions

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const logger = require('./modules/logger.js');
+const { readdirSync } = require('fs');
+const { permLevels } = require('./config.js');
+
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -11,6 +17,7 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
+
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -30,6 +37,8 @@ client.on('interactionCreate', async interaction => {
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
+	const eventName = file.split('.')[0];
+	logger.log(`Loading Event: ${eventName}. 👌`, 'log');
 	const event = require(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
@@ -38,5 +47,18 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
-
+// Generate a cache of client permissions for pretty perm names in commands.
+const levelCache = {};
+for (let i = 0; i < permLevels.length; i++) {
+	const thisLevel = permLevels[i];
+	levelCache[thisLevel.name] = thisLevel.level;
+}
+// To reduce client pollution we'll create a single container property
+// that we can attach everything we need to.
+client.container = {
+	commands,
+	aliases,
+	slashcmds,
+	levelCache,
+};
 client.login(token);
--- work-in-progress/keyword.js
+++ work-in-progress/keyword.js
@@ -20,16 +20,22 @@ let messageContent = [
   `*${randomLine}*`
 ];
 
-// Only respond to messages containing the word "satan", "baphomet", "devil", "beelzebub", or "lucifer"
-(/\bsatan\b|\bbaphomet\b|\bdevil\b|\bbeelzebub\b|\blucifer\b/gmi)
+const fs = require('fs');
+const data = fs.readFileSync('static/quotes.txt', 'utf8');
+const readMe = data.split(/\r?\n/);
+const lineCount = readMe.length;
+const randomLineNumber = Math.floor(Math.random() * lineCount);
+const randomLine = readMe[randomLineNumber];
 
-if (keyWords.match(/\bsatan\b|\bbaphomet\b|\bdevil\b|\bbeelzebub\b|\blucifer\b/i)) {
-  await lib.discord.channels['@0.0.6'].messages.create({
-    channel_id: context.params.event.channel_id,
-    content: messageContent.join('\n'),
-    message_reference: {
-      message_id: context.params.event.id
-    }
-  });
+
+
+{
+  (message.content.includes(new RegExp(/\bsatan\b|\bbaphomet\b|\bdevil\b|\bbeelzebub\b|\blucifer\b/gmi))); {
+    message.channel.send('Ay yo! You said **my name** !',
+      '',
+      'You summoned me, what do you want? A quote?',
+      '',
+      `*${randomLine}*`,
+    );
+  }
 }
-*/
