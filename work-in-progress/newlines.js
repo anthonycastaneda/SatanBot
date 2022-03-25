f4ae6b88e9a0f0546ff926e315373ac93edf249b
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-03-25 17:49:29 -0500

    Removed old commands

--- work-in-progress/newlines.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-console.log('start');
-fs.readFileSync('/Users/anthonycastaneda/SatanBot/work-in-progress/quotes.txt').toString().split('\n').forEach(function(line, index, arr) {
-	if (index === arr.length - 1 && line === '') { return; }
-	console.log(index + ' ' + line);
-});
-console.log('end');
--- work-in-progress/rando-quote.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-async function getQuoteRandom() {
-    const response = await fetch('https://api.quotable.io/random');
-    const data = await response.json();
-    return (`${data.content} - ${data.author}`);
-}
-getQuoteRandom().then(console.log());
