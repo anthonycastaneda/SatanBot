aa1b464f631f8c8fd056e7f34c8ec67308ebc985
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-03-26 23:10:37 -0500

    new

--- commands/satan.js
+++ commands/satan.js
@@ -13,19 +13,19 @@ module.exports = {
 			.setColor('#d10404')
 			.setTitle('The Satanic Temple ~~(𖤐)~~')
 			.setURL('https://thesatanictemple.com')
-			.setImage('https://img.shields.io/static/v1?label=hail%20satan&message=𖤐&color=d10404&style=for-the-badge&logo=riseup&logoColor=d10404')
+			.setImage('https://anthonycastaneda.com/img/baph-poster.png')
 			.setAuthor('Helel ben Shachar', 'https://cdn.shopify.com/s/files/1/0428/0465/t/58/assets/satan-logo-red.png', 'https://ordained.satanicministry.com')
 			.setDescription('Information about TSTs active campaigns can be found [here](https://thesatanictemple.com/pages/campaigns). ```The Mission Of The Satanic Temple Is To Encourage Benevolence And Empathy, Reject Tyrannical Authority, Advocate Practical Common Sense, Oppose Injustice, And Undertake Noble Pursuits```')
 			.setThumbnail('https://anthonycastaneda.com/img/binarysatan.png')
 			.addFields(
-				{ name: '♀️', value: 'Consistent with our tenets that call for bodily autonomy and acting in accordance with best scientific evidence, The Satanic Temple religiously objects to many of the restrictions that states have enacted that interfere with abortion access as well as other related issues that affect members religious right', inline: false },
+				{ name: '♀️', value: 'Consistent with our tenets that call for bodily autonomy and acting in accordance with best scientific evidence, The Satanic Temple religiously objects to many of the restrictions that states have enacted that interfere with abortion access as well as other related issues that affect members religious rights', inline: false },
 				{ name: '🚩', value: 'Protect Mental Health, Particularly In The Area Of So-Called “Repressed Memories.”', inline: false },
 				{ name: '🏫', value: 'The Protect Children Project Addresses Abuses That Take Place In Public Schools That Are Perpetrated By School Officials And Are Deemed Legal.', inline: false },
 				{ name: '🚫', value: 'No Gods, No Masters', inline: true },
 				{ name: '🤘', value: 'Faithless and Proud', inline: true }
 
 			)
-			.setTimestamp('timestamp')
+
 			.setFooter('H̷̨̨̪͙̳̳̲̬͓͗̔̊͠Ā̵͚̎͋̚İ̴̠̠̟̃̅̏̌̇͝͝L̶̡̛͎̠̬̹̼̟͉͈̋͑͂̐̃͘̚ ̶̛̣̟͗̕͝͝S̶̳̺̖̯̤̼͚͍̆͒͛̄̽̚͝Ạ̵̭̣̟̬̹̉̉͋͊̄͑͝T̴̡̜̫̹̠̋̇̚ͅĄ̴̅̅̈̾N̶͔̆͌̀̾͌', 'https://st.depositphotos.com/1062321/4991/v/950/depositphotos_49917567-stock-illustration-pentagram-icon.jpg');
 
 		return interaction.editReply({ embeds: [embedSatan] });
