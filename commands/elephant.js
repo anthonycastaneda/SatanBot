4f3cb38567c5bb49750ad1e2d329c21fadce6545
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-03-29 16:50:13 -0500

    two new commands

---
+++ commands/elephant.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+var axios = require("axios");
+
+var config = {
+  method: "get",
+  url: "https://meme-api.herokuapp.com/gimme/babyelephantgifs",
+  headers: {},
+};
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("elephant")
+    .setDescription("Baby Elephant Gifs"),
+  async execute(interaction) {
+    await interaction.deferReply();
+    axios(config).then((response) => {
+      response.data;
+      {
+        const elephantEmbed = new MessageEmbed()
+          .setColor("0xd22b2b")
+          .setTitle(response.data.title)
+          .setImage(response.data.url)
+          .setDescription(" ");
+        return interaction.editReply({ embeds: [elephantEmbed] });
+      }
+    });
+  },
+};
---
+++ commands/funnygif.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+var axios = require("axios");
+
+var config = {
+  method: "get",
+  url: "https://meme-api.herokuapp.com/gimme/gifs",
+  headers: {},
+};
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("funny")
+    .setDescription("Funny Gifs"),
+  async execute(interaction) {
+    await interaction.deferReply();
+    axios(config).then((response) => {
+      response.data;
+      {
+        const funnyEmbed = new MessageEmbed()
+          .setColor("0xd22b2b")
+          .setTitle(response.data.title)
+          .setImage(response.data.url)
+          .setDescription(" ");
+        return interaction.editReply({ embeds: [funnyEmbed] });
+      }
+    });
+  },
+};
