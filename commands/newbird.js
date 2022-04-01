c28eb085366a9564ea2ea849ef31af40c6e3bcf1
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-01 06:07:10 -0500

    obj orig

--- commands/newbird.js
+++ commands/newbird.js
@@ -34,7 +34,7 @@ module.exports = {
                     const page = json.query
                     // console.log(page.pages[0]);
                     const obj = page;
-                    console.log(obj.pages[0].original);
+                    console.log(obj.original.source);
                  } );
 				{const birdEmbed = new MessageEmbed()
 					.setColor('0xd22b2b')
