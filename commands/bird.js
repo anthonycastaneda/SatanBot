d19c84db2776eaa1b5827a752d7ab043d627be6a
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-02 05:25:30 -0500

    bird-final

--- commands/bird.js
+++ commands/bird.js
@@ -26,27 +26,38 @@ module.exports = {
                  let birdSci = String(birdJson[0].sciName)
                  let birdLoc = String(birdJson[0].locName)
                  let birdDate = String(birdJson[0].obsDt)
-				 const wtf = require('wtf_wikipedia')
-                 wtf.extend(require('wtf-plugin-api'))
-                 wtf.extend(require('wtf-plugin-image'))
-               
-                 let doc = await wtf.fetch(`${birdName}`)
-                 let img = doc.mainImage()
-                 console.log(img)
-                  .catch((err) => {
-                    console.log(err)
-                  })
-                  .catch((error) => {
-                    console.log(error)
-                  });
-                  const birdEmbed = new MessageEmbed()
-                    .setColor("0xd22b2b")
-                    .setTitle("The Most Recent Walker County Bird")
-                    .setDescription(
-                      `**Common Name**:  ${birdName}\n**Scientific Name**:  ${birdSci}\n**Location**:  ${birdLoc}\n**Date**:  ${birdDate}`
-                    )
-                    .setImage(`${img.src()}`);
-                  return interaction.editReply({ embeds: [birdEmbed] });
+				 //Image Search
+                 const { XMLHttpRequest } = require ('w3c-xmlhttprequest');
+                 var xhr = new XMLHttpRequest();
+                 xhr.withCredentials = true;
+
+                 xhr.open(
+                   "GET",
+                   `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages&titles=${birdName}&redirects=1&formatversion=2&piprop=name%7Coriginal`
+                 );
+                 xhr.setRequestHeader(
+                   "Cookie",
+                   "GeoIP=US:TX:Huntsville:30.65:-95.58:v4"
+                 );
+
+                 xhr.send();
+                 xhr.addEventListener("readystatechange", function () {
+                   if (this.readyState === 4) {
+                     const json = this.responseText;
+                     const jsonParsed = JSON.parse(json);
+                     const imgUrl = jsonParsed.query.pages[0].original.source;
+                     console.log(imgUrl);
+
+                     const birdEmbed = new MessageEmbed()
+                       .setColor("0xd22b2b")
+                       .setTitle("The Most Recent Walker County Bird")
+                       .setDescription(
+                         `**Common Name**:  ${birdName}\n**Scientific Name**:  ${birdSci}\n**Location**:  ${birdLoc}\n**Date**:  ${birdDate}`
+                       )
+                       .setImage(`${imgUrl}`);
+                     return interaction.editReply({ embeds: [birdEmbed] });
+                   }
+                 });
                 },
                )
              },
