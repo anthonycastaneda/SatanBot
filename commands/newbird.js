c7c443922d2d2688f8c3e79e29c3fdc798443bd0
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-02 00:33:22 -0500

    async

--- commands/newbird.js
+++ commands/newbird.js
@@ -21,40 +21,33 @@ module.exports = {
 		await interaction.deferReply();
 		axios(config)
 			.then((response) => {
-				const birdJson = response.data;
-                let birdName = String(birdJson[0].comName);
-                let birdSci = String(birdJson[0].sciName);
-                let birdLoc = String(birdJson[0].locName);
-                let birdDate = String(birdJson[0].obsDt);
-                // Image Portion
-                const wtf = require("wtf_wikipedia");
-                wtf.extend(require("wtf-plugin-api"));
-                wtf.extend(require("wtf-plugin-image"));
-                const doc =  wtf.fetch (`${birdName}`);
-                console.log(doc);
-                const docImage = await wtf.fetch('Black Vulture');
-                let img = docImage.mainImage();
-                console.log(img.src());
-                
-
-                
-                
-                
-                const birdEmbed = new MessageEmbed()
-                        .setColor("0xd22b2b")
-                        .setTitle("The Most Recent Walker County Bird")
-                        .setDescription(
-                          `**Common Name**:  ${birdName}\n**Scientific Name**:  ${birdSci}\n**Location**:  ${birdLoc}\n**Date**:  ${birdDate}`
-                        )
-                        .setImage(`${img.src()}`);
-                      return interaction.editReply({ embeds: [birdEmbed] });
-                    
-                  })
+                 const birdJson = response.data
+                 let birdName = String(birdJson[0].comName)
+                 let birdSci = String(birdJson[0].sciName)
+                 let birdLoc = String(birdJson[0].locName)
+                 let birdDate = String(birdJson[0].obsDt)
+				 const wtf = require('wtf_wikipedia')
+                 wtf.extend(require('wtf-plugin-api'))
+                 wtf.extend(require('wtf-plugin-image'))
+               
+                 let doc = await wtf.fetch(`${birdName}`)
+                 let img = doc.mainImage()
+                 console.log(img)
                   .catch((err) => {
-                    console.log(err);
+                    console.log(err)
                   })
                   .catch((error) => {
-                    console.log(error);
-                  })
+                    console.log(error)
+                  });
+                  const birdEmbed = new MessageEmbed()
+                    .setColor("0xd22b2b")
+                    .setTitle("The Most Recent Walker County Bird")
+                    .setDescription(
+                      `**Common Name**:  ${birdName}\n**Scientific Name**:  ${birdSci}\n**Location**:  ${birdLoc}\n**Date**:  ${birdDate}`
+                    )
+                    .setImage(`${img.src()}`);
+                  return interaction.editReply({ embeds: [birdEmbed] });
                 },
-            };
+               )
+             },
+            }
