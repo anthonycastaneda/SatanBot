8d5026ef75f7804ae9012d57b63f88006fd743f1
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-03 04:52:17 -0500

    flag json

---
+++ commands/flag.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('flag')
+		.setDescription('Getchu a Flag 🏴‍☠️'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		fetch('https://flagcdn.com/en/codes.json')
+			.then(response => response.json());
+            console.log(response.json);
+           /* {
+				{const flagEmbed = new MessageEmbed()
+					.setColor('0xd22b2b')
+					.setTitle('Flags of the World 🌎')
+					.setImage(`${dogImageResponse}`)
+					.setDescription('');
+				return interaction.editReply({ embeds: [dogEmbed] });
+				}
+			},*/
+			
+	},
+ };
+
+
+
+
+// https://flagcdn.com/h120/
