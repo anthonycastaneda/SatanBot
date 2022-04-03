5e86bddd1b2f6fa037b40881213b25ef10d4ac50
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-03 17:14:34 -0500

    burger of the day

---
+++ commands/bobsburgers.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('burger-of-the-day')
+		.setDescription('Gets a Random Burger of the Day'),
+	async execute(interaction) {
+		await interaction.deferReply();
+        function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+        const rndInt = randomIntFromInterval(1, 162)
+        console.log(rndInt)
+        const url = `https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${rndInt}`;
+		await fetch(url)
+			.then((response) => {
+				const burgerResponse = response.data
+                console.log(burgerResponse)
+				/*{const burgerEmbed = new MessageEmbed()
+					.setColor('0xd22b2b')
+					.setTitle('You are a Good Boy')
+					.setImage(`${dogImageResponse}`)
+					.setDescription('Yes you are!');
+				return interaction.editReply({ embeds: [burgerEmbed] });
+				}*/
+			},
+			);
+	},
+};
+
+
+
