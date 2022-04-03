490d913ceddbf3efc5f9ee6bbb371e69b6d13cab
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-03 07:48:32 -0500

    pictureUrl

--- commands/hobbes.js
+++ commands/hobbes.js
@@ -1,12 +1,21 @@
-const  request = require('request')
-const cheerio = require('cheerio')
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const request = require("request");
+const cheerio = require("cheerio");
 
-function sendImage(date, id) {
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  //load the page
-  request(
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('hobbes')
+	.setDescription('Get a Calvin and Hobbes Comic'),
+	async execute(interaction) {
+    await interaction.deferReply();
+    function sendImage(date, id) {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    //load the page
+    request(
     {
       uri:
         "http://www.gocomics.com/calvinandhobbes/" +
@@ -21,7 +30,15 @@ function sendImage(date, id) {
       //get the picture
       let pictureUrl = $(".item-comic-image img").attr("src");
       console.log(pictureUrl);
-    
+      return pictureUrl
+        }
+    );
+          /*{const hobbesEmbed = new MessageEmbed()
+          .setColor("0xd22b2b")
+          .setTitle("Hobbes")
+          .setImage(`${pictureUrl}`);
+		  return interaction.editReply({ embeds: [hobbesEmbed] });
+		  }*/
     }
-  );
+},
 }
