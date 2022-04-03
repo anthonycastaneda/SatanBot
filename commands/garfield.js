4c05b3e0ff9e95ca566b5e8049a0e7929aa43eaa
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-02 23:50:10 -0500

    remote

--- commands/garfield.js
+++ commands/garfield.js
@@ -9,17 +9,17 @@ module.exports = {
     async execute(interaction) {
     await interaction.deferReply();{
     
-      const dir = "./static/GnG";
+      const dir = "https://satanbot.anthonycastaneda.com/GnG/";
       randomFile(dir, (err, file) => {
     
         const garfieldEmbed = new MessageEmbed()
-        .setColor("0xd22b2b")
-        .setTitle("Garfield Minus Garfield")
-        .setDescription(
-          "This is a journey deep into the mind of an isolated young everyman as he fights a losing battle against loneliness and depression in a quiet American suburb"
-        )
-        .setImage(`${file}`)
-        .setURL('https://garfieldminusgarfield.net');
+          .setColor("0xd22b2b")
+          .setTitle("Garfield Minus Garfield")
+          .setDescription(
+            "This is a journey deep into the mind of an isolated young everyman as he fights a losing battle against loneliness and depression in a quiet American suburb"
+          )
+          .setImage(`https://satanbot.anthonycastaneda.com/GnG/${file}`)
+          .setURL("https://garfieldminusgarfield.net");
       return interaction.editReply({ embeds: [garfieldEmbed] });
         })
     }
