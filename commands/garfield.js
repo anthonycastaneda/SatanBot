8528ad57297b4b2fd05ed7604f28fc30b22f283d
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-02 20:29:13 -0500

    array?

--- commands/garfield.js
+++ commands/garfield.js
@@ -17,7 +17,7 @@ module.exports = {
     feed.items.forEach(item => {
     let regex = /<img.*?src="(.*?)"/;
     let src = regex.exec(item.content)[1];   
-    console.log(typeof src);
+    console.log(src[0]);
     
     //const random = [Math.floor(Math.random() * linkArray.length)];
     //console.log(linkArray[random]);
