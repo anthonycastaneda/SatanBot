08ec5b4b18356134f307a3a892f5b0f636827f21
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-03 18:58:01 -0500

    bofd

--- commands/bobsburgers.js
+++ commands/bobsburgers.js
@@ -8,11 +8,9 @@ module.exports = {
 		.setDescription('Gets a Random Burger of the Day'),
 	async execute(interaction) {
 		await interaction.deferReply();
-        function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-        }
+        function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)}
         const rndInt = randomIntFromInterval(1, 162)
-        console.log(rndInt)
         const url = `https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${rndInt}`;
 		await fetch(url)
         .then((result) => result.json())
@@ -20,20 +18,18 @@ module.exports = {
          const outputJson = (output)
          const burger = outputJson.burgers[0]
          const epTitle = outputJson.episodeName
-         console.log(burger,epTitle)
-          })
-          .catch((err) => console.error(err));
-                
-				/*{const burgerEmbed = new MessageEmbed()
-					.setColor('0xd22b2b')
-					.setTitle('You are a Good Boy')
-					.setImage(`${dogImageResponse}`)
-					.setDescription('Yes you are!');
-				return interaction.editReply({ embeds: [burgerEmbed] });
-				}*/
-			},
-			
-	};
+         {const burgerEmbed = new MessageEmbed()
+            .setColor("0xd22b2b")
+            .setTitle(`**Episode**: ${epTitle}`)
+            .setURL("https://bobs-burgers.fandom.com/wiki/Burger_of_the_Day")
+            .setImage(
+              "https://static.wikia.nocookie.net/bobsburgerpedia/images/4/47/Bobs-Burgers-Wiki-Animate-Burger_003.gif/revision/latest/scale-to-width-down/168?cb=20140116100604"
+            )
+            .setDescription(`**Burger of the Day**: ${burger}`);}
+			return interaction.editReply({ embeds: [burgerEmbed] });})
+        	
+        }
+	}
 
 
 
