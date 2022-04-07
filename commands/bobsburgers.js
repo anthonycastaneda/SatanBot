47871e8272f8dc202416d39603ebd0b9d3900d5a
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-04-06 19:26:25 -0500

    test

--- commands/bobsburgers.js
+++ commands/bobsburgers.js
@@ -12,25 +12,24 @@ module.exports = {
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
     const rndInt = randomIntFromInterval(1, 162);
-    const url = (`https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${rndInt}`);
+    const url = `https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${rndInt}`;
     await fetch(url)
       .then((result) => result.json())
       .then((output) => {
-        const outputJson = (output)
+        const outputJson = output;
         const burger = outputJson.burgers[0];
         const epTitle = outputJson.episodeName;
-        console.log(outputJson);
         {
           const burgerEmbed = new MessageEmbed()
             .setColor("0xd22b2b")
             .setTitle(`**Episode**: ${epTitle}`)
             .setURL("https://bobs-burgers.fandom.com/wiki/Burger_of_the_Day")
             .setImage(
-              "https://static.wikia.nocookie.net/bobsburgerpedia/images/4/47/Bobs-Burgers-Wiki-Animate-Burger_003.gif"
+              "https://static.wikia.nocookie.net/bobsburgerpedia/images/4/47/Bobs-Burgers-Wiki-Animate-Burger_003.gif/revision/latest/scale-to-width-down/168?cb=20140116100604"
             )
             .setDescription(`**Burger of the Day**: ${burger}`);
-          return interaction.editReply({ embeds: [burgerEmbed] });
         }
+        return interaction.editReply({ embeds: [burgerEmbed] });
       });
   },
 };
