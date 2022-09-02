e5d223f276a6a9963444a3732eefc1afda722fe0
Anthony Castaneda <anthony.castaneda@gmail.com>
2022-09-01 21:32:18 -0500

    Moving Guild ID

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,13 +6,13 @@ const { clientId, guildId, token } = require("./config.json");
 
 const commands = [];
 const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 
@@ -20,6 +20,6 @@ const rest = new REST({ version: "9" }).setToken(token);
 
 
 rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log("Successfully registered application commands."))
+    .catch(console.error);
